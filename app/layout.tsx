9db49ef2de5f4e9522e95715b0daa9d647800f91
Obedson <115647152+richie997@users.noreply.github.com>
2024-01-21 23:42:34 +0100

    setup

---
+++ app/layout.tsx
@@ -0,0 +1,32 @@
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700", "800", "900"],
+  variable: "--font-poppins",
+});
+
+export const metadata: Metadata = {
+  title: "Event",
+  description: "Event requirement",
+  icons: {
+    icon: "/assets/images/logo.svg",
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body className={poppins.variable}>{children}</body>
+      </html>
+    </ClerkProvider>
+  );
+}
